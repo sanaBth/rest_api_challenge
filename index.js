9c68adb7a582d6c3c4838f54c6eb60b3fa113b32
sanaBth <thayria20@yahoo.com>
2021-10-24 16:19:32 +0100

    send mail text format

--- index.js
+++ index.js
@@ -18,8 +18,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/apitodo',todoApi);
 app.use('/apiuser',userApi);
 
+const dotenv = require('dotenv');
+dotenv.config();
+var nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass:process.env.PASSWORD,
+  }
+});
 
+var mailOptions = {
+  from: 'sanabenthayer30@gmail.com',
+  to: 'sanabenthayer30@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
 app.listen(process.env.port || 
   4000,function(){
