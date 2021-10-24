c5b54451cbe00baa025249f78bfd65e06578c9c7
sanaBth <thayria20@yahoo.com>
2021-10-24 17:34:57 +0100

    fichiers organisés

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const User = require ('./models/user');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const todoApi = require('./routes/todoRoutes');
-
 const userApi = require('./routes/userRoutes');
+const mailApi = require('./routes/mailRoutes');
 const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/todo');
@@ -17,34 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/apitodo',todoApi);
 app.use('/apiuser',userApi);
+app.use('/sendmail',mailApi);
 
-const dotenv = require('dotenv');
-dotenv.config();
-var nodemailer = require('nodemailer');
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass:process.env.PASSWORD,
-  }
-});
-
-var mailOptions = {
-  from: 'sanabenthayer30@gmail.com',
-  to: 'sanabenthayer30@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!',
-  html:`<h2>testing send mail format html</h2>`
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
 
 app.listen(process.env.port || 
   4000,function(){
